91143926a284526bb1db7fe3baf26d447ab804cf
sarahmdht <sarahmdht@gmail.com>
2023-06-15 16:12:27 -0700

    hello world code correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * return: always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
