98a69ed92adef7d33fd7b6aea7d64aa8be6ffe0c
sarahmdht <sarahmdht@gmail.com>
2023-06-16 12:22:52 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,6 +7,8 @@
 int main(void)
 {
 int n;
+{
+for (n = 0; n <= 16 ; n++)
 if (n < 10)
 {
 putchar('0' + n);
@@ -14,6 +16,7 @@ putchar('0' + n);
 else
 {
 putchar(87 + n);
+}
 }
 putchar('\n');
 return (0);
