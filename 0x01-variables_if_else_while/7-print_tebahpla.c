f7b52ca7d45860ed4652b99b78d3d0fac07953f6
sarahmdht <sarahmdht@gmail.com>
2023-06-16 12:12:22 -0700

    letters reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char c;
+for (c = 'z' ; c >= 'a' ; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
