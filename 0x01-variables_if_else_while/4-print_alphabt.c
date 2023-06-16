5cc39f67a766d35294c511efeabf6307883d3237
sarahmdht <sarahmdht@gmail.com>
2023-06-16 11:02:48 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-	char c;
+char c;
 
 	for (c = 'a' ; c <= 'z' ; c++)
 	{
-	  if ((c == 'q' || c == 'e') != 1)
-	    {
-	      putchar(c);
-	    }
+		if ((c == 'q' || c == 'e') != 1)
+		{
+			putchar(c);
+		}
+
 	}
 	putchar('\n');
 	return (0);
