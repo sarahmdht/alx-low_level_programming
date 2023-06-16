e908b27ea93fbc6607f28f003294a227c1933456
sarahmdht <sarahmdht@gmail.com>
2023-06-16 12:57:59 -0700

    doubles

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+int a;
+int b;
+int c;
+int d;
+for ( a = 0 ; a < 10 ; a++)
+{	
+for (b = 0 ; b <= 10 ; b++)
+{
+for (c = 0 ; c < 10 ; c++)
+{
+for ( d = 0 ; d < 10 ; d++)
+{
+putchar('0' + a);
+putchar('0' + b);
+putchar(32);
+putchar('0' + c);
+putchar('0' + d);
+if (!(a == 9 && b == 8))
+{
+putchar(',');
+putchar(32);
+}
+d++;
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
