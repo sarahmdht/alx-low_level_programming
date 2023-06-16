836179d93ea7b0ee7dd422acc66062ef8128f1a5
sarahmdht <sarahmdht@gmail.com>
2023-06-16 13:01:15 -0700

    doubles

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,13 +12,13 @@ int a;
 int b;
 int c;
 int d;
-for ( a = 0 ; a < 10 ; a++)
-{	
+for (a = 0 ; a < 10 ; a++)
+{
 for (b = 0 ; b <= 10 ; b++)
 {
 for (c = 0 ; c < 10 ; c++)
 {
-for ( d = 0 ; d < 10 ; d++)
+for (d = 0 ; d < 10 ; d++)
 {
 putchar('0' + a);
 putchar('0' + b);
