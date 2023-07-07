6c5bf9fa7a1acc2a69bd5f8562c3d982a5bed2c0
sarahmdht <sarahmdht@gmail.com>
2023-07-07 11:59:14 -0700

    library done

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - Write a function that checks for lowercase character.
+ * @c: alphabet character
+ * Return: Nothing.
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - for abslute value
+ * @i: integer
+ * Return: i
+ */
+int _abs(int i)
+{
+if (i < 0)
+i = i * -1;
+return (i);
+}
