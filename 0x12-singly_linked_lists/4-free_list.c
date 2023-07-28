82642b76be55300ee101be6f7f01399aa61735de
sarahmdht <sarahmdht@gmail.com>
2023-07-28 12:34:15 -0700

    tasks 3 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees a linked list
+ * @head: list to be freed
+ */
+void free_list(list_t *head)
+{
+list_t *t;
+while (head)
+{
+t = head->next;
+free(head->str);
+free(head);
+head = t;
+}
+}
